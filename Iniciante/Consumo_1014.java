965e9a051cf3f0690df703c2aee95416ead4de16
WilliamNunes905 <williamtrybe@gmail.com>
2024-08-13 20:11:11 -0300

    ✨   Iniciante|1014 - Consumo

---
+++ Iniciante/Consumo_1014.java
@@ -0,0 +1,28 @@
+package Iniciante;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/*
+Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km),
+e o total de combustível gasto (em litros).
+
+----- Entrada -----
+O arquivo de entrada contém dois valores: um valor inteiro X representando a distância total percorrida (em Km),
+e um valor real Y representando o total de combustível gasto, com um dígito após o ponto decimal.
+
+----- Saída -----
+Apresente o valor que representa o consumo médio do automóvel com 3 casas após a vírgula, seguido da mensagem "km/l".
+ */
+
+public class Consumo_1014 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat decimal = new DecimalFormat("0.000");
+        int distanciaTotal = scanner.nextInt();
+        double combustivelGasto = scanner.nextDouble();
+
+        double consumoMedio = distanciaTotal/combustivelGasto;
+        System.out.println(decimal.format(consumoMedio) + " km/l");
+    }
+}

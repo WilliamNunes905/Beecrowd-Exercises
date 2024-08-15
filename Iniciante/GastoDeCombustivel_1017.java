2b5f2cd7fb1676c9dacaa7fa32843eea1eb2585a
WilliamNunes905 <williamtrybe@gmail.com>
2024-08-15 10:29:44 -0300

    ✨   Iniciante|1017 Gasto de Combustível

---
+++ Iniciante/GastoDeCombustivel_1017.java
@@ -0,0 +1,36 @@
+package Iniciante;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/*
+Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem,
+ao utilizar um automóvel que faz 12 KM/L. Para isso, ele gostaria que você o auxiliasse através de um simples programa.
+Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e a velocidade média durante a mesma (em km/h).
+Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam necessários. Mostre o valor com 3 casas decimais após o ponto.
+
+|------ Entrada ------|
+O arquivo de entrada contém dois inteiros. O primeiro é o tempo gasto na viagem (em horas) e o segundo é a velocidade média durante a mesma (em km/h).
+
+|------ Saída ------|
+Imprima a quantidade de litros necessária para realizar a viagem, com três dígitos após o ponto decimal
+
+|---Exemplo de Entrada---| |---Exemplo de Saída---|
+            10                         70.833
+            85
+ */
+
+public class GastoDeCombustivel_1017 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat decimal = new DecimalFormat("0.000");
+
+        int tempoGasto = scanner.nextInt();
+        int velocidadeMedia = scanner.nextInt();
+        int kmPorLitro = 12;
+
+        double distanciaPercorrida = velocidadeMedia * tempoGasto;
+        double litrosGastos = distanciaPercorrida / kmPorLitro;
+        System.out.println(decimal.format(litrosGastos));
+    }
+}

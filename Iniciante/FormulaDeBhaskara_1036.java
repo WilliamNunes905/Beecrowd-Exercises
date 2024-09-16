1c121ec4d6fdc2d716d095d6ccfb0ad43290d192
WilliamNunes905 <williamtrybe@gmail.com>
2024-09-16 15:28:31 -0300

    📚 Adicionando a instrução do exercicio

--- Iniciante/FormulaDeBhaskara_1036.java
+++ Iniciante/FormulaDeBhaskara_1036.java
@@ -4,8 +4,26 @@ import java.util.Scanner;
 import java.lang.Math;
 import java.text.DecimalFormat;
 
-public class FormulaDeBhaskara_1036 {
+/*
+Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes,
+mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.
 
+|----- Entrada -----|
+Leia três valores de ponto flutuante (double) A, B e C.
+
+|----- Saída -----|
+Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular".
+Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente conforme exemplo abaixo.
+Imprima sempre o final de linha após cada mensagem.
+
+|---- Exemplos de Entrada ----	Exemplos de Saída ----|
+10.0 20.1 5.1                   R1 = -0.29788
+                                R2 = -1.71212
+
+0.0 20.0 5.0                    Impossivel calcular
+ */
+
+public class FormulaDeBhaskara_1036 {
   public static void main(String[] args) {
     Scanner teclado = new Scanner(System.in);
     DecimalFormat decimal = new DecimalFormat("0.00000");
